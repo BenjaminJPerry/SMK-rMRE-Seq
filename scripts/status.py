e5608caa539bf45a59bdea6be204ecf691f7f9fa
Benjamin J Perry <ben.perry@agresearch.co.nz>
2023-08-28 16:32:50 +1200

    tweaking status.py

--- scripts/status.py
+++ scripts/status.py
@@ -1,23 +1,15 @@
-#!/usr/bin/env python3
-# Forked from: https://github.com/rusalkaguy/snakemake-slurm-module/blob/master/slurm-status.py
-
-import re
+#!/usr/bin/env python
 import subprocess
-import shlex
 import sys
-import time
 
-
-jobid = sys.argv[1] # original from https://github.com/Snakemake-Profiles/slurm
-#jobid = jobid.split()[3] # needed when --parsable not indicated with sbatch
+jobid = sys.argv[1]
 
 output = str(subprocess.check_output("sacct -j %s --format State --noheader | head -1 | awk '{print $1}'" % jobid, shell=True).strip())
 
-running_status=["PENDING", "CONFIGURING", "COMPLETING", "RUNNING", "SUSPENDED", "BadConstraints"]
+running_status=["PENDING", "CONFIGURING", "COMPLETING", "RUNNING", "SUSPENDED"]
 if "COMPLETED" in output:
-  print("success")
+    print("success")
 elif any(r in output for r in running_status):
-  print("running")
+    print("running")
 else:
-  print("failed")
-
+    print("failed")
